0e39cd4c149fa648adc604f77bcce4f0fbfb8562
Austen Stone <austenstone@github.com>
2022-07-08 10:37:15 -0400

    temp

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -3,21 +3,11 @@ import { h } from "preact";
 import { useState } from "preact/hooks";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
-interface CounterProps {
-  start: number;
-}
-
-export default function Counter(props: CounterProps) {
+export default function Search(props) {
   const [count, setCount] = useState(props.start);
   return (
     <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count - 1)} disabled={!IS_BROWSER}>
-        -1
-      </button>
-      <button onClick={() => setCount(count + 1)} disabled={!IS_BROWSER}>
-        +1
-      </button>
+      <p>{123}</p>
     </div>
   );
 }
--- spotify.ts
+++ spotify.ts
@@ -1,6 +1,126 @@
 
 import { getCookies, setCookie } from "https://deno.land/std@0.144.0/http/cookie.ts";
 
+export interface SearchResult {
+    artists: Artists;
+    tracks:  Tracks;
+}
+
+export interface Artists {
+    href:     string;
+    items:    ArtistsItem[];
+    limit:    number;
+    next:     string;
+    offset:   number;
+    previous: null;
+    total:    number;
+}
+
+export interface ArtistsItem {
+    external_urls: ExternalUrls;
+    followers:     Followers;
+    genres:        string[];
+    href:          string;
+    id:            string;
+    images:        Image[];
+    name:          string;
+    popularity:    number;
+    type:          ArtistType;
+    uri:           string;
+}
+
+export interface ExternalUrls {
+    spotify: string;
+}
+
+export interface Followers {
+    href:  null;
+    total: number;
+}
+
+export interface Image {
+    height: number;
+    url:    string;
+    width:  number;
+}
+
+export enum ArtistType {
+    Artist = "artist",
+}
+
+export interface Tracks {
+    href:     string;
+    items:    TracksItem[];
+    limit:    number;
+    next:     string;
+    offset:   number;
+    previous: null;
+    total:    number;
+}
+
+export interface TracksItem {
+    album:             Album;
+    artists:           Artist[];
+    available_markets: string[];
+    disc_number:       number;
+    duration_ms:       number;
+    explicit:          boolean;
+    external_ids:      ExternalIDS;
+    external_urls:     ExternalUrls;
+    href:              string;
+    id:                string;
+    is_local:          boolean;
+    name:              string;
+    popularity:        number;
+    preview_url:       null | string;
+    track_number:      number;
+    type:              PurpleType;
+    uri:               string;
+}
+
+export interface Album {
+    album_type:             AlbumTypeEnum;
+    artists:                Artist[];
+    available_markets:      string[];
+    external_urls:          ExternalUrls;
+    href:                   string;
+    id:                     string;
+    images:                 Image[];
+    name:                   string;
+    release_date:           string;
+    release_date_precision: ReleaseDatePrecision;
+    total_tracks:           number;
+    type:                   AlbumTypeEnum;
+    uri:                    string;
+}
+
+export enum AlbumTypeEnum {
+    Album = "album",
+    Single = "single",
+}
+
+export interface Artist {
+    external_urls: ExternalUrls;
+    href:          string;
+    id:            string;
+    name:          string;
+    type:          ArtistType;
+    uri:           string;
+}
+
+export enum ReleaseDatePrecision {
+    Day = "day",
+    Year = "year",
+}
+
+export interface ExternalIDS {
+    isrc: string;
+}
+
+export enum PurpleType {
+    Track = "track",
+}
+
 export class SpotifyApi {
     baseAccountUrl = "https://accounts.spotify.com";
     baseApiUrl = "https://api.spotify.com";
@@ -57,12 +177,13 @@ export class SpotifyApi {
         return response;
     }
 
-    apiRequest(req: Request, url: string): Promise<Response> {
+    apiRequest(req: Request, url: string, method?: string): Promise<Response> {
         const maybeAccessToken = getCookies(req.headers)["spotify_token"];
         if (!maybeAccessToken) {
             return Promise.reject(new Error("No access token"));
         }
         return fetch(`${this.baseApiUrl}/${url}`, {
+            method: method || "GET",
             headers: {
                 Authorization: `Bearer ${maybeAccessToken}`
             }
@@ -70,7 +191,16 @@ export class SpotifyApi {
     }
 
     getUserProfile(req: Request): Promise<Response> {
-        return spotifyApi.apiRequest(req, 'v1/me');
+        return this.apiRequest(req, 'v1/me');
+    }
+
+    search(req: Request, query: string, type: string[], limit: number = 20, offset: number = 0): Promise<Response> {
+        return this.apiRequest(req, `v1/search?${new URLSearchParams({
+            q: query,
+            type: type?.join(','),
+            limit: limit.toString(),
+            offset: offset.toString(),
+        }).toString()}`);
     }
 }
 
