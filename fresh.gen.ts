028719e3d623f01950c8023618b1be3f2f7078b7
Austen Stone <austenstone@github.com>
2022-07-07 23:34:19 -0400

    gen

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,20 +2,16 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/callback.tsx";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/login.tsx";
+import * as $0 from "./routes/callback.tsx";
+import * as $1 from "./routes/index.tsx";
+import * as $2 from "./routes/login.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/callback.tsx": $2,
-    "./routes/index.tsx": $3,
-    "./routes/login.tsx": $4,
+    "./routes/callback.tsx": $0,
+    "./routes/index.tsx": $1,
+    "./routes/login.tsx": $2,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
